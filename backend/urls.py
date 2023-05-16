900ba668ddbbe37816e68431a9ab88f1b72c8db4
fabian <fa.flores@duocuc.cl>
2023-05-15 21:46:52 -0400

    la sopa ta lista

--- backend/urls.py
+++ backend/urls.py
@@ -5,4 +5,10 @@ from ventas import views
 urlpatterns = [
     path('cliente/', views.rf_cliente),
     path('cliente/<int:rut_cli>', views.rf_cliente_pk),
+    path('soap_service/', views.my_soap_application),
+    path('soap_service_ferreteria/', views.pruebaFerreteria),
+    
+
+    ###path('soap_service_consumir/', views.SoapList.as_view()),
+    
 ]
--- ventas/views.py
+++ ventas/views.py
@@ -100,3 +100,68 @@ def rf_proveedor_pk(request,rut_cli):
         
 
     return JSONResponse(proveedor.errors, status=400)
+
+
+### Soap
+
+from django.views.decorators.csrf import csrf_exempt
+from spyne.application import Application
+from spyne.decorator import rpc
+from spyne.model.primitive import Unicode, Integer
+from spyne.protocol.soap import Soap11
+from spyne.server.django import DjangoApplication
+from spyne.service import ServiceBase
+
+# http://127.0.0.1:9010/ventas/soap_service/
+# http://127.0.0.1:9010/ventas/soap_service/?WSDL
+class SoapService(ServiceBase):
+    @rpc(Unicode(nillable=False), _returns=Unicode)
+    def hello(ctx, name):
+        return 'Hello, {}'.format(name)
+
+    @rpc(Integer(nillable=False), Integer(nillable=False), _returns=Integer)
+    def sum(ctx, a, b):
+        return int(a + b)
+
+
+soap_app = Application(
+    [SoapService],
+    tns='django.soap.example',
+    in_protocol=Soap11(validator='lxml'),
+    out_protocol=Soap11(),
+)
+
+django_soap_application = DjangoApplication(soap_app)
+my_soap_application = csrf_exempt(django_soap_application)
+
+
+# pruebas Soap 222
+class SoapServiceFerreteria(ServiceBase):
+    @rpc(Unicode(nillable=False), _returns=Unicode)
+    def mandarHola(ctx, nombre):
+        return 'Hola Profesor, {}'.format(nombre)
+
+    @rpc(Integer(nillable=False), Integer(nillable=False), Integer(nillable=False), _returns=Integer)
+    def ejeSumar(ctx, a, b, c):
+        return int(a + b + c)
+soap_app_ferreteria = Application(
+    [SoapServiceFerreteria],
+    tns='django.soap.example',
+    in_protocol=Soap11(validator='lxml'),
+    out_protocol=Soap11(),
+)
+
+django_soap_ferreteria = DjangoApplication(soap_app_ferreteria)
+pruebaFerreteria = csrf_exempt(django_soap_ferreteria)
+
+
+####  Consumir
+###from suds.client import Client
+###from suds.cache import NoCache
+###class SoapList(APIView):
+   ### def get(self, request, format=None):
+      ###  my_client = Client('http://127.0.0.1:8000/ventas/soap_service/?WSDL', cache=NoCache())
+        ###print("WSDL Metodos : ", my_client)
+        ###stResult = 'Function hello: ' +  str(my_client.service.hello('Harrys el magnifico'))
+        ###stResult = stResult + '<br>'+ 'Function sum: ' + str(my_client.service.sum(10, 25))
+        ###return HttpResponse(stResult)
